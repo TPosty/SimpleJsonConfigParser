bfa5e745bc62cc9dab9e22c1515b8c6c32bc94b9
Tyler <tpostuma@proton.me>
2024-04-02 23:08:16 -0500

    Changed version number

--- SimpleJsonConfigParser/ConfigReader.py
+++ SimpleJsonConfigParser/ConfigReader.py
@@ -4,21 +4,21 @@ import json
 
 class config_reader():
 
-    def __init__(self, config_path):
-        self.config_path = config_path
+    def __init__(self, base_directory):
+        self.base_directory = base_directory
 
     def create_config(self, config_name: str, *args):
         # Config file name (what to save the file as)
         # Path - Where to save the config file
         
         # First we need to check if a file with that name exists already on the user's OS
-        if os.path.exists(os.path.join(self.config_path, config_name)):
+        if os.path.exists(os.path.join(self.base_directory, config_name)):
             print("Could not create the config due to the file already being there!")
             return
         else:
             # Proceed with the rest of the creation of the config file
             try:
-                with open(os.path.join(self.config_path, config_name), 'w') as config:
+                with open(os.path.join(self.base_directory, config_name), 'w') as config:
                     print("Created config file!")
                     # Check if there are other arguments, currently only supports argument for setup data.
                     if args:
@@ -143,9 +143,9 @@ class config_reader():
     def remove_section(self, section_name: str):
         pass
 
-    def get(self, parent_section: str, config_key: str):
+    def get(self, config_path: str, parent_section: str, config_key: str):
         try:
-            with open(str(self.config_path)) as config:
+            with open(config_path) as config:
                 config_data = json.load(config)
                 data = config_data[f'{parent_section}']
                 for key, value in data.items():
